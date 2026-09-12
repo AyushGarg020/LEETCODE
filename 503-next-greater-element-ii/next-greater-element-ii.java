3e168bded7ef1f853ba23997ff066666be3debbd
Ayush Garg <ayush021007@gmail.com>
2026-09-12 16:38:46 +0530

    Time: 10 ms (78.27%) | Memory: 48.5 MB (48.06%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,19 +1,20 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
-        int[] arr = new int[n];
-        Arrays.fill(arr, -1);
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> st = new Stack<Integer>();
+        int[] res = new int[n];
+        Arrays.fill(res, -1);
+        
         for(int i=0; i<2*n; i++) {
             int val = nums[i%n];
-            while(!stack.isEmpty() && nums[stack.peek()] < val) {
-                arr[stack.peek()] = val;
-                stack.pop();
+            while(!st.isEmpty() && nums[st.peek()]<val) {
+                res[st.peek()] = val;
+                st.pop();
             }
             if(i<n) {
-                stack.push(i);
+                st.push(i);
             }
         }
-        return arr;
+        return res;
     }
 }
