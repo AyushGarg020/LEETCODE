8d807c20f75b335df3376255d4485052ed14bb99
Ayush Garg <ayush021007@gmail.com>
2026-09-14 20:33:07 +0530

    Time: 28 ms (76.74%) | Memory: 108.7 MB (61.27%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,5 +1,23 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=0; i<nums.length; i++){
+            if(!set.add(nums[i])){
+                return true;
+            }
+            if(set.size()>k){
+                set.remove(nums[i-k]);
+            }
+        }
+
+        return false;
+    }
+}
+
+/*
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
         var map = new HashMap<Integer, Integer>();
         for(int i=0; i<nums.length; i++) {
             int previous = map.getOrDefault(nums[i], -999999);
@@ -13,7 +31,7 @@ class Solution {
         return false;
     }
 }
-/*
+
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         var set = new HashSet<Integer>();
