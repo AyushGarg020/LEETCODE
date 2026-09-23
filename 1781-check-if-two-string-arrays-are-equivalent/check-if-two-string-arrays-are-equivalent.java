d46fb8bf8f9ab525e44bf0407323e32ae8e27fe0
Ayush Garg <ayush021007@gmail.com>
2026-09-23 16:18:24 +0530

    Time: 1 ms (90.16%) | Memory: 43 MB (81.53%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -1,7 +1,24 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder str1 = new StringBuilder();
+        StringBuilder str2 = new StringBuilder();
+
+        for(String s : word1)
+            str1.append(s);
+        
+        for(String s : word2)
+            str2.append(s);
+        
+        return str1.toString().equals(str2.toString());
+    }
+}
+
+/*
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
         String str1 = String.join("", word1);
         String str2 = String.join("", word2);
         return str1.equals(str2);
     }
 }
+*/
